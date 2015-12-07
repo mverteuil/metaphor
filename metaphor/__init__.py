ab96941e69915c23f879c9c4b5c81b264e7398b6
M. de Verteuil <onceuponajooks@gmail.com>
2015-12-06 19:55:37 -0500

    Tidy up comparator

--- metaphor/__init__.py
+++ metaphor/__init__.py
@@ -3,8 +3,7 @@ from __future__ import print_function
 from collections import deque
 from datetime import timedelta
 from glob import glob
-from pprint import pprint
-import itertools
+from itertools import groupby
 import os
 import time
 
@@ -14,43 +13,37 @@ import imageio
 import ssim
 
 
-def compare_preview_image_modified(current_image, preview_image):
-    current_modified_epoch = os.path.getmtime(current_image)
-    current_modified = dateparser.parse(time.ctime(current_modified_epoch))
+class CompareImageSimilarityAndModifiedDate(object):
+    __slots__ = ['image']
 
-    next_modified_epoch = os.path.getmtime(preview_image)
-    next_modified = dateparser.parse(time.ctime(next_modified_epoch))
-    diff = next_modified - current_modified
-    return diff
-
-
-def compare_preview_image_similarity(current_image, preview_image):
-    similarity = ssim.compute_ssim(Image.open(current_image), Image.open(preview_image))
-    return similarity
-
-
-class AdjacentKey(object):
-    __slots__ = ['obj']
-
-    def __init__(self, obj):
-        self.obj = obj
+    def __init__(self, image):
+        self.image = image
 
     def __eq__(self, other):
-        similarity = compare_preview_image_similarity(self.obj, other.obj)
-        time_interval = compare_preview_image_modified(self.obj, other.obj)
-        reasonable_match = similarity > 0.55 or (similarity < 0.5 and time_interval < timedelta(seconds=30))
-
-        #print(self.obj, other.obj, reasonable_match, similarity, time_interval)
-
+        reasonable_match = self.similar_image(other) or self.similar_modified_date(other)
         if reasonable_match:
-            self.obj = other.obj
+            self.image = other.image
         return reasonable_match
+
+    def similar_image(self, other):
+        self.similarity = ssim.compute_ssim(Image.open(self.image), Image.open(other.image))
+        return self.similarity > 0.55
+
+    def similar_modified_date(self, other):
+        current_modified_epoch = os.path.getmtime(self.image)
+        current_modified = dateparser.parse(time.ctime(current_modified_epoch))
+
+        next_modified_epoch = os.path.getmtime(other.image)
+        next_modified = dateparser.parse(time.ctime(next_modified_epoch))
+
+        time_interval = next_modified - current_modified
+        return self.similarity < 0.5 and time_interval < timedelta(seconds=30)
 
 
 if __name__ == '__main__':
     here = os.path.dirname(__file__)
     image_files = glob(os.path.join(here, '..', 'test_images', '*.png'))
-    sequences = [list(g) for k, g in itertools.groupby(image_files, key=AdjacentKey)]
+    sequences = [tuple(g) for _, g in groupby(image_files, key=CompareImageSimilarityAndModifiedDate)]
     for sequence_id, sequence in enumerate(sequences):
         sequence_filename = 'sequence_{sequence_id}.gif'.format(**locals())
         image_sequence = [imageio.imread(frame) for frame in sequence]
