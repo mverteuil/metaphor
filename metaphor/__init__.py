d97d30c38c6f4fac0e4aaf93268942339d5fe8f1
M. de Verteuil <onceuponajooks@gmail.com>
2015-12-06 19:36:03 -0500

    Demonstrably working ;)

--- metaphor/__init__.py
+++ metaphor/__init__.py
@@ -1,47 +1,27 @@
 from __future__ import print_function
 
 from collections import deque
+from datetime import timedelta
 from glob import glob
+from pprint import pprint
 import itertools
 import os
 import time
 
-from dateutil import parser as dateparser
 from PIL import Image
-import imagehash
+from dateutil import parser as dateparser
+import imageio
 import ssim
-
-
-def preview_list(iterable):
-    """ Creates a new iterable that includes a preview of the next item as iteration occurs. """
-    # Fork the iterable
-    original, preview = itertools.tee(iterable)
-    # Advance the preview iterable to be one ahead of the original
-    next(preview)
-    return zip(original, preview)
 
 
 def compare_preview_image_modified(current_image, preview_image):
     current_modified_epoch = os.path.getmtime(current_image)
     current_modified = dateparser.parse(time.ctime(current_modified_epoch))
 
-    if preview_image:
-        next_modified_epoch = os.path.getmtime(preview_image)
-        next_modified = dateparser.parse(time.ctime(next_modified_epoch))
-        diff = next_modified - current_modified
-    else:
-        next_modified = None
-        diff = None
-
-    print(current_modified)
+    next_modified_epoch = os.path.getmtime(preview_image)
+    next_modified = dateparser.parse(time.ctime(next_modified_epoch))
+    diff = next_modified - current_modified
     return diff
-
-
-
-def compare_preview_image_hash(current_image, preview_image, hash_function=imagehash.average_hash):
-    current = hash_function(Image.open(current_image))
-    preview = hash_function(Image.open(preview_image))
-    print(current, preview)
 
 
 def compare_preview_image_similarity(current_image, preview_image):
@@ -51,11 +31,17 @@ def compare_preview_image_similarity(current_image, preview_image):
 
 class AdjacentKey(object):
     __slots__ = ['obj']
+
     def __init__(self, obj):
         self.obj = obj
+
     def __eq__(self, other):
         similarity = compare_preview_image_similarity(self.obj, other.obj)
-        reasonable_match = similarity > 0.55
+        time_interval = compare_preview_image_modified(self.obj, other.obj)
+        reasonable_match = similarity > 0.55 or (similarity < 0.5 and time_interval < timedelta(seconds=30))
+
+        #print(self.obj, other.obj, reasonable_match, similarity, time_interval)
+
         if reasonable_match:
             self.obj = other.obj
         return reasonable_match
@@ -64,5 +50,9 @@ class AdjacentKey(object):
 if __name__ == '__main__':
     here = os.path.dirname(__file__)
     image_files = glob(os.path.join(here, '..', 'test_images', '*.png'))
-    groups = [list(g) for k, g in itertools.groupby(image_files, key=AdjacentKey)]
-    print(groups)
+    sequences = [list(g) for k, g in itertools.groupby(image_files, key=AdjacentKey)]
+    for sequence_id, sequence in enumerate(sequences):
+        sequence_filename = 'sequence_{sequence_id}.gif'.format(**locals())
+        image_sequence = [imageio.imread(frame) for frame in sequence]
+        imageio.mimwrite(sequence_filename, image_sequence, format='GIF', loop=0, duration=1.5)
+        print(sequence_filename, ' saved')
